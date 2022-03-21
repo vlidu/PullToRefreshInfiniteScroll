dce5e6a30012fc4768c852d2ec3ad8c25e2d5f7b
Alexandre VLADOVICH-RELJA <avladovichrelja@sqli.com>
2022-03-21 16:07:53 +0100

    Initial Commit

---
+++ PullToRefreshPOCSwiftUI/PullToRefreshPOCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshPOCSwiftUIApp.swift
+//  PullToRefreshPOCSwiftUI
+//
+//  Created by Alexandre VLADOVICH RELJA on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshPOCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
